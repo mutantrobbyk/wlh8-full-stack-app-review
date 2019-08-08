b6d2af054fb3f7e0a49846ea9d8384d3c810c19d
Robert Knowles <robertknowles@Roberts-MacBook-Pro.local>
2019-08-08 10:58:59 -0600

    front end skeleton

--- __instructor-code__/src/App.js
+++ __instructor-code__/src/App.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 import Dashboard from './components/Dashboard/Dashboard'
 import Landing from './components/Landing/Landing'
 import Nav from './components/Nav/Nav'
 import Transaction from './components/Transaction/Transaction'
 import Transfer from './components/Transfer/Transfer'
 
-
 function App() {
   return (
     <div className="App">
-        <Nav/>
-        <Dashboard/>
-        <Landing/>
-        <Transaction/>
-        <Transfer/>
+      <Nav />
+      <Dashboard />
+      <Landing />
+      <Transaction />
+      <Transfer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
